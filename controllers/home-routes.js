1d707d9d65ee0189c7bb2be687a2b49d84d40f38
Carlos Rojas <carlosrb206@gmail.com>
2021-12-02 14:04:09 -0500

    user sessions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,20 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// render login
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
+  res.render('login');
+});
+
+
+// render homepage
 router.get('/', (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: [
       'id',
@@ -38,5 +50,7 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+
 
 module.exports = router;
